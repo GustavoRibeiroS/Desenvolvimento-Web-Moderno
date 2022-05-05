46c287546d895a20902bef90d1be3779a32f0d41
Gustavo Ribeiro <gustavo.ribeiro1056@gmail.com>
2022-05-04 21:09:21 -0300

    Hello Java

---
+++ .vscode/fiapJava/javaVar2.java
@@ -0,0 +1,16 @@
+public class javaVar2 {
+    
+    public static void main(String[] args) {
+
+        int x = 10;
+        double d = x;
+        long l = x;
+        float f = x;
+        short s = 20;
+        x = s;
+
+        double valorDouble = 9.99;
+        int valorInt = (int) valorDouble; //Entre o () coloca o tipo de dados que estou forçando a fazer na conversão
+        System.out.println(valorInt);
+    }
+}
