1dd314c11f289bf70d32cb276d7631eb57d4d023
Gustavo Ribeiro <gustavo.ribeiro1056@gmail.com>
2022-05-05 14:21:05 -0300

    destructuring1

---
+++ fundamentos-js/destructuring1.js
@@ -0,0 +1,22 @@
+const pessoa = {
+    nome:'Ana',
+    idade: 5,
+    endereco: {
+        logradouro: "Rua dos bobos",
+        numero: 7
+    }
+}
+
+console.log(pessoa);
+
+const { nome, idade } = pessoa
+console.log(nome, idade);
+
+const { nome: n, idade: i } = pessoa
+console.log(n, i);
+
+const { sobrenome, humorBom = true } =  pessoa
+console.log(sobrenome, humorBom);
+
+const { endereco: { logradouro, numero, cep } } = pessoa
+console.log(logradouro, numero, cep);
