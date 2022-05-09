0ee51175bb34cc34c12a0632c4a87bf2c9e208d8
Gustavo Ribeiro <gustavo.ribeiro1056@gmail.com>
2022-05-09 15:18:44 -0300

    ternarios

---
+++ fundamentos-js/ternario.js
@@ -0,0 +1,4 @@
+const resultado = nota => nota >=7 ? 'aprovado' : 'reprovado' //Expressão relacional, primeiro se nota for verdadeiro (>=7) retorna aprovado, se não for retorna reprovado. (tipo um If / else)
+
+console.log(resultado (7.1));
+console.log(resultado (6.9));
