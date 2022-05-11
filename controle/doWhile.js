08b2555fa61b8fd93329514f0bbbf703a1941787
Gustavo Ribeiro <gustavo.ribeiro1056@gmail.com>
2022-05-11 15:33:02 -0300

    doWhile

---
+++ controle/doWhile.js
@@ -0,0 +1,14 @@
+function getInteiroAleatorioEntre(min, max) {
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+let opcao = -1
+
+ do{
+    opcao = getInteiroAleatorioEntre(-1, 10)
+    console.log(`Opção escilhida foi ${opcao}.`) //entre craze `
+    
+}while (opcao !=-1)
+
+console.log("Até a próxima");
