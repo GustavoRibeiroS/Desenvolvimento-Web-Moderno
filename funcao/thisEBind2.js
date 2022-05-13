58eb3dc8f7a78eaf8b9a94d4a30bb268ca3b18ad
Gustavo Ribeiro <gustavo.ribeiro1056@gmail.com>
2022-05-13 16:06:28 -0300

    Update thisEBind2.js

--- funcao/thisEBind2.js
+++ funcao/thisEBind2.js
@@ -3,10 +3,10 @@ function Pessoa() {
 
     const self = this //atribui a palavra this, para a constante self
 
-    setInterval(function() {   //Dispara uma outra função a partir de outro intervalo que você passou
-        self.idade++ //cria o laço de repetição somando a 1 para idade
+    setInterval(function() {   // setInterval dispara uma outra função a partir de um intervalo que você passar
+        self.idade++ // No o laço de repetição somando a 1 para idade
         console.log(self.idade);
-    }/*.bind(this) */, 1000) // Intervalo de 1000 mili segundos (a cada 1s a função será disparada)
+    }/*.bind(this) */, 1000) // Intervalo declarado de 1000 milissegundos (a cada 1s a função será disparada)
 }
 
 new Pessoa
