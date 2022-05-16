3f4c100a5213236607fb6cbb90baf148a8690e8f
Gustavo Ribeiro <gustavo.ribeiro1056@gmail.com>
2022-05-16 17:56:07 -0300

    arrow3

---
+++ funcao/arrowFunction3.js
@@ -0,0 +1,21 @@
+let comparaComThis = function (param) {
+    console.log(this === param); //this é estritamente igual ao parametro que foi passado. Se atribuir um atributo ao this no corpo de uma função irá mudar o escopo global
+}
+
+comparaComThis(global)
+
+const obj = {}
+    comparaComThis = comparaComThis.bind(obj) //agora o this compara com o obj, não mais para o escopo global
+    
+comparaComThis(global)
+comparaComThis(obj)
+
+let comparaComThisArrow = param => console.log(this === param)
+comparaComThis(global)
+
+//para apontar para o arrow no módulo corrente
+comparaComThisArrow(module.exports)
+
+comparaComThisArrow = comparaComThisArrow.bind(obj) //não funciona, pois o this continua apontando para o objeto no qual a função foi escrita/ O this de uma função Arrow é associada ao contexto da rm que função foi escrita
+comparaComThisArrow(obj)
+comparaComThisArrow(module.exports)
