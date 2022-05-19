515067cc7601c13bdd9001b3d6ad77b70f5461f4
Gustavo Ribeiro <gustavo.ribeiro1056@gmail.com>
2022-05-19 15:34:35 -0300

    Update callApply.js

--- funcao/callApply.js
+++ funcao/callApply.js
@@ -16,7 +16,7 @@ console.log(produto.getPreco());
 
 const carro = {preco: 49990, desc: 0.20}
 console.log(getPreco.call(carro));
-console.log(getPreco.apply(carro));
+console.log(getPreco.apply(carro)); //mesmo parâmetro para chamar
 
 console.log(getPreco.call(carro, 0.17, '$')); //primeiro contexto, depois os parâmetros que ele irá passar
 console.log(getPreco.apply(carro, [0.17, '$'])); // no aplly ele deve estra dentro de um arrey
