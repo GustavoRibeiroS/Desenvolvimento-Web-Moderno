dc8c05cc53219c9cb2c549fc87bd5d1c0d34dcc5
Gustavo Ribeiro <gustavo.ribeiro1056@gmail.com>
2022-05-18 21:20:35 -0300

    construtora

---
+++ funcao/desafioFuncaoConstrutora.js
@@ -0,0 +1,19 @@
+class Pessoa {
+    constructor (nome){
+        this.nome = nome;
+    }
+    falar(){
+        console.log(`Este é meu nome ${this.nome}`)
+    }
+}
+
+function Pessoa(nome) {
+    this.nome = nome
+
+    this.falar= function(){
+        console.log(`Este é meu nome ${this.nome}`)
+    }
+}
+
+const p1 = new Pessoa("João")
+p1.falar()
