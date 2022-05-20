6301ade4096837f5e0a4108ba75ab7fb490686f6
Gustavo Ribeiro <gustavo.ribeiro1056@gmail.com>
2022-05-20 17:49:05 -0300

    somaSubtraiDivideMultiplica

---
+++ funcao/exerciciosFuncao/somaSubtraiDivideMultiplica.js
@@ -0,0 +1,8 @@
+function valores (valor1, valor2) {
+    console.log(`A Soma de ${valor1}+${valor2} é: ${valor1 + valor2}`); 
+    console.log(`A Subtração de ${valor1}-${valor2} é: ${valor1 - valor2}`); 
+    console.log(`A Divisão de ${valor1}/${valor2} é: ${valor1 / valor2}`); 
+    console.log(`A Multiplicação de ${valor1}*${valor2} é: ${valor1 * valor2}`); 
+    
+}
+valores(4,2)
