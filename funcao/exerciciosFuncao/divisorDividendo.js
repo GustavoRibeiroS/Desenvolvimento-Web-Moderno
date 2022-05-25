a69c4fd9546ad45758284bd1342e24f0885fc898
Gustavo Ribeiro <gustavo.ribeiro1056@gmail.com>
2022-05-25 15:36:50 -0300

    divisorDividendo

---
+++ funcao/exerciciosFuncao/divisorDividendo.js
@@ -0,0 +1,5 @@
+function divisorDividendo(dividendo, divisor){
+    console.log("Resultado: " + Math.floor(`${dividendo / divisor}`));
+    console.log(`Resto: ${dividendo % divisor}`);
+}
+divisorDividendo(10,3)
