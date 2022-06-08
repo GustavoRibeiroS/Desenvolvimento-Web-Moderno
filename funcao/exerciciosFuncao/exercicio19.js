0ee62c1b26505069582fa6b275dad7fc2a2fc299
Gustavo Ribeiro <gustavo.ribeiro1056@gmail.com>
2022-06-08 18:07:29 -0300

    cardápio - ex 19

---
+++ funcao/exerciciosFuncao/exercicio19.js
@@ -0,0 +1,27 @@
+function cardapio(codigo,quantidade) {
+    switch (codigo) {
+        case 100:
+            return console.log(`O valor a ser pago por ${quantidade} Cachorro Quente é: ${quantidade * 3.00}`);
+        case 200:
+            return console.log(`O valor a ser pago por ${quantidade} Hamburguer Simples é: ${quantidade * 4.00}`); 
+        case 300:
+            return console.log(`O valor a ser pago por ${quantidade} Cheeseburguer é: ${quantidade * 5.50}`); 
+        case 400:
+            return console.log(`O valor a ser pago por ${quantidade} Bauru é : ${quantidade * 7.50}`); 
+        case 500:
+            return console.log(`O valor a ser pago por ${quantidade} Refrigerante é: ${quantidade * 3.50}`); 
+        case 600:
+            return console.log(`O valor a ser pago por ${quantidade} Suco é: ${quantidade * 2.80}`);
+
+        default:
+            console.log("Produto não existente");
+            break;
+    }
+}
+cardapio(100,2)
+cardapio(200,2)
+cardapio(300,2)
+cardapio(400,2)
+cardapio(500,2)
+cardapio(600,2)
+cardapio(650,2)
