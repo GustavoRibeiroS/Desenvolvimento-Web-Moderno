70315fd8672b5eb67546c5ba7d1e3e360cd6715d
Gustavo Ribeiro <gustavo.ribeiro1056@gmail.com>
2022-06-14 16:22:45 -0300

    1 ao 50 - ex25

---
+++ funcao/exerciciosFuncao/exercicio25.js
@@ -0,0 +1,18 @@
+function imprima() {
+    var i = 0;
+    var num = 0
+while (i < 50) {
+    console.log(num += 1);
+    i++
+}
+}
+imprima()
+
+// usando for
+/*
+function contador () {
+    for (let i = 1; i<=50; i++) {
+        console.log(i);
+    }
+}
+contador()*/
