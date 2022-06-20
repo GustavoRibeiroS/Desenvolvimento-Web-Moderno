6529186b25bd1e3cde121ea0cea1441d86549ef8
Gustavo Ribeiro <gustavo.ribeiro1056@gmail.com>
2022-06-20 16:14:11 -0300

    encontrar [maiorMenor] - ex30

---
+++ funcao/exerciciosFuncao/exercicio30.js
@@ -0,0 +1,19 @@
+function maiorMenor(vetor) {
+    let maior = vetor[0]
+    let menor = vetor[0]
+
+    for (let i = 0; i < vetor.length; i++) {
+        if (vetor[i] > maior) {
+            maior = vetor[i]
+        }else if (vetor[i] < menor) {
+            menor = vetor[i]
+        }
+    }
+    return [maior, menor]
+
+}
+const vetor = [10, -2, -7, 5, 7, 3, 1, 3, 11, 35, 20, 6, 9]
+console.log(maiorMenor(vetor)); 
+
+/*console.log(`Maior: ${maior} `);
+console.log(`Menor: ${menor}`);*/
