e4eacb6e2ce5718c75134b83773f0c4c2a6f5397
Gustavo Ribeiro <gustavo.ribeiro1056@gmail.com>
2022-06-21 15:45:55 -0300

    mediaVetores - ex32

---
+++ funcao/exerciciosFuncao/exercicio.32.js
@@ -0,0 +1,12 @@
+function calcularMedia(vetor) {
+    let soma = 0
+
+    for (let i = 0; i < vetor.length; i++) {
+        soma += vetor[i]
+    }
+    let media = soma/vetor.length
+    console.log(`${media}`);
+}
+vetor = [1, 2, 3, 4, 5]
+
+calcularMedia(vetor)
