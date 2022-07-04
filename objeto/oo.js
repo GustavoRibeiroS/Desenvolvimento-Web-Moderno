2c626bbc1de814378f1e968c3c35da9f82c8cecb
Gustavo Ribeiro <gustavo.ribeiro1056@gmail.com>
2022-07-04 18:22:32 -0300

    Introdução OO
    aula 114

---
+++ objeto/oo.js
@@ -0,0 +1,21 @@
+//Procedural
+processamento (valor1, valor2, valor3)
+
+//OO - (Objeto tem valores, atributos internos do objeto)
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){ //Tentro do objeto, tem comportamentos 
+        //....comport.
+    }
+
+}
+
+objeto.processamento() //objeto invocado a partit do dado
+
+//Principios Importantes
+//1. abstração (Trazer um objeto do mundo real e traduzir para o sistema. Ex. carro como obj)
+//2. encapsulamento (Ter os detalhes de implementação escontido, mas apresentar bem os detalhes da aplicação)
+//3. herança (prototype) (Características que passam de um objeto de "classificação superior" para outro )
+//4. polimorfismo (Conceito generico, que pode subtituir conceitos a partir de uma variável genérica. Ex. Em um carro pode ser uma ferrari, um fiat, um celta...)
