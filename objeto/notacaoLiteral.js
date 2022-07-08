224fcaf5cc1ef6bcfaec2a80733f922f6c4d6964
Gustavo Ribeiro <gustavo.ribeiro1056@gmail.com>
2022-07-08 14:49:49 -0300

    Create notacaoLiteral.js

---
+++ objeto/notacaoLiteral.js
@@ -0,0 +1,16 @@
+const a = 1
+const b = 2
+const c = 3
+
+const obj1 = {a : a, b : b, c: c}
+const obj2 = {a, b, c}
+console.log(obj1, obj2); // iguais, independente da forma escrita
+
+const nomeAttr = 'Nota'
+const valorAttr = 7.83
+
+const obj3 = {}
+obj3[nomeAttr] = valorAttr
+console.log(obj3);
+
+const obj4 = {[]}
