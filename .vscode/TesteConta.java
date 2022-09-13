7016e7bb3b752038a67bb6e24d1876e5d4bd035b
Gustavo Ribeiro <gustavo.ribeiro1056@gmail.com>
2022-09-13 15:46:33 -0300

    classe Java

---
+++ .vscode/TesteConta.java
@@ -0,0 +1,10 @@
+public class TesteConta {
+    public static void main(String[] args) {
+        
+        Conta contaCorrente = new Conta();
+        Conta contaPupaca = new Conta();
+        Conta contaInvestimento = new Conta();
+    }
+}
+
+//Conta está armazenando 3 objetos em memória (corrente, poupanca e investimento)
