cc8c543406a941b6df3fcec5ab2e3b03104784a5
Gustavo Ribeiro <gustavo.ribeiro1056@gmail.com>
2022-09-13 15:37:51 -0300

    atlz

--- .vscode/fiapJava/helloJava.java
+++ .vscode/fiapJava/helloJava.java
@@ -2,6 +2,7 @@ public class helloJava {
 
     public static void main(String[] args) {
 
+        String nome = "Tavino";
         int idade = 10;
         double preco = 10.5;
         char sexo = 'M';
@@ -11,6 +12,9 @@ public class helloJava {
         System.out.println(preco);
         System.out.println(sexo);
         System.out.println(temFilhos);       
+
+        System.out.println(nome);       
+        System.out.println("Ola, " + nome);       
         
     }
 }
