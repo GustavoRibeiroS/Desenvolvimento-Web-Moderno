eeb8a299b1b685f596461fdbf751f9e030b425a3
Gustavo Ribeiro <gustavo.ribeiro1056@gmail.com>
2022-09-14 21:41:06 -0300

    if else java

---
+++ .vscode/fiapJava/Aprovacao.java
@@ -0,0 +1,13 @@
+public class Aprovacao {
+
+    public static void main(String[] args) {
+        int nota = 75;
+        
+        if (nota >= 70) {
+            System.out.println("Aluno Aprovado!");
+        } else {
+            System.out.println("Aluno Reprovado");
+        }
+
+    }
+}
